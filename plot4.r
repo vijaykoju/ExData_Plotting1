8c2d75700125217f9d8d978b48c744b9f3cc0764
Vijay Koju <vjk8736@gmail.com>
2016-11-12 21:46:55 -0600

    Update plot4.r

--- plot4.r
+++ plot4.r
@@ -2,7 +2,6 @@ dataFile <- "household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-#str(subSetData)
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
